0d18c385afa089d72361215dabe540ed4a8580c8
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-05-25 19:27:11 +0600

    updated

--- WhoIsIt.cpp
+++ WhoIsIt.cpp
@@ -2,17 +2,23 @@
 
 using namespace std;
 
-struct Student {
+class Student {
+public:
     int id;
     string name;
     char section;
     int totalMarks;
+    Student(int id, string name, char section, int totalMarks) {
+        this->id = id;
+        this->name = name;
+        this->section = section;
+        this->totalMarks = totalMarks;
+    }
 };
-
-bool compareStudents(const Student& s1, const Student& s2) {
-    if (s1.totalMarks == s2.totalMarks)
-        return s1.id < s2.id;
-    return s1.totalMarks > s2.totalMarks;
+bool compareStudents(const Student& a, const Student& b) {
+    if (a.totalMarks != b.totalMarks)
+        return a.totalMarks > b.totalMarks;
+    return a.id < b.id;
 }
 
 int main() {
@@ -20,14 +26,15 @@ int main() {
     cin >> T;
 
     while (T--) {
-        vector<Student> students(3);
-        
+        vector<Student> students;
         for (int i = 0; i < 3; ++i) {
-            cin >> students[i].id >> students[i].name >> students[i].section >> students[i].totalMarks;
+            int id, totalMarks;
+            string name;
+            char section;
+            cin >> id >> name >> section >> totalMarks;
+            students.push_back(Student(id, name, section, totalMarks));
         }
-
         sort(students.begin(), students.end(), compareStudents);
-
         cout << students[0].id << " " << students[0].name << " " << students[0].section << " " << students[0].totalMarks << endl;
     }
 
