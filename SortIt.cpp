caa563e47818e4172950b11bb84848e637fc35a3
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-05-25 18:03:25 +0600

    one done

---
+++ SortIt.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> A(N);
+    for(int i = 0; i < N; ++i) {
+        cin >> A[i];
+    }
+    
+    sort(A.begin(), A.end());
+    for(int i = 0; i < N; ++i) {
+        cout << A[i] << (i == N-1 ? "\n" : " ");
+    }
+    
+    sort(A.begin(), A.end(), greater<int>());
+    for(int i = 0; i < N; ++i) {
+        cout << A[i] << (i == N-1 ? "\n" : " ");
+    }
+    
+    return 0;
+}
