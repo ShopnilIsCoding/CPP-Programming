7d5a79b623bef88c2d507df3f98552da5aaff9ac
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-08-26 15:26:06 +0600

    done

---
+++ Data Structures/Assignment-4 (BST)/Sum_Without_Leaf.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* input_tree() {
+    int val;
+    cin >> val;
+    Node* root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node*> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty()) {
+        Node* p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node* myLeft;
+        Node* myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+bool is_leaf(Node* node) {
+    return node->left == NULL && node->right == NULL;
+}
+int sum_non_leaf(Node* root) {
+    if (root == NULL)
+        return 0;
+    
+    int sum = 0;
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        Node* p = q.front();
+        q.pop();
+        if (!is_leaf(p)) {
+            sum += p->val;
+        }
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+
+    return sum;
+}
+
+int main() {
+    Node* root = input_tree();
+    cout << sum_non_leaf(root) << endl;
+    return 0;
+}
